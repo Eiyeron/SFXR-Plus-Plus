23811b9125f1f5fbb2b5386d11e497d7f76938a1
Eiyeron <eiyeron@gmail.com>
2013-05-02 10:18:08 +0200

    Added MiGLayout Libraries for Soundbox

---
+++ src/com/Eiyeron/SFXRPP/FX.java
@@ -0,0 +1,5 @@
+package com.Eiyeron.SFXRPP;
+
+public enum FX {
+	PICKUP, LASER, EXPLOSION, POWERUP, HURT, JUMP, BIP
+}
---
+++ src/com/Eiyeron/SFXRPP/WaveForm.java
@@ -0,0 +1,5 @@
+package com.Eiyeron.SFXRPP;
+
+public enum WaveForm {
+	SQUARE, SAWTOOTH, SINE, NOISE, TRIANGLE, TAN, WHISTLE, BREAKER
+}
